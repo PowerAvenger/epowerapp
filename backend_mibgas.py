a04747bdfd1c2ccbb664682f3c4af168362a66ba
Jose <>
2025-09-30 08:58:27 +0200

    a

--- backend_mibgas.py
+++ backend_mibgas.py
@@ -64,7 +64,7 @@ def graficar_qs(df_mg_q):
         tickformat="%b\n%Y",  # etiquetas: abreviatura del mes y año
         showgrid=True,
         gridcolor="lightgrey",
-        gridwidth=.5
+        gridwidth=.1
     )
 
     # 3) Contenido de cada fila del tooltip (nombre del Q y su valor)
