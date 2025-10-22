eb83f2f72c0ead068a2fe8f83646bdd5c8ab62f5
Jose <>
2025-10-22 12:33:55 +0200

    a

--- pages/mibgas.py
+++ pages/mibgas.py
@@ -28,9 +28,28 @@ graf_qs = graficar_qs(df_mg_q)
 df_mg_da = filtrar_por_producto(df_mibgas_base, 'GDAES_D+1')
 print('mibgas da')
 print(df_mg_da)
+
+df_medias = df_mg_da.groupby("año_entrega", as_index=False)["precio_gas"].mean()
+df_medias["precio_gas"] = df_medias["precio_gas"].round(2)
+# Convertir a texto con coma decimal
+df_medias["precio_str"] = df_medias["precio_gas"].astype(str).str.replace('.', ',')
+
 graf_da_corrido = graficar_da_corrido(df_mg_da)
 graf_da_comparado = graficar_da_comparado(df_mg_da)
 
+
+#LAYOUT++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
 st.write(graf_qs)
-st.write(graf_da_corrido)
+
+with st.container():
+    col1,col2 = st.columns([.9,.1])
+    with col1:
+        st.write(graf_da_corrido)
+    with col2:
+        st.metric("Precio medio gas 2024 (€)", df_medias.loc[df_medias["año_entrega"] == 2024, "precio_str"].values[0])
+        st.metric("Precio medio gas 2025 (€)", df_medias.loc[df_medias["año_entrega"] == 2025, "precio_str"].values[0])
+
 st.write(graf_da_comparado)
