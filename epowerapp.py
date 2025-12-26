c01d77141ef4fae20a09abb8f711b2c67fe8c1d1
Jose <>
2025-12-26 08:31:50 +0100

    añadido keyacces2

--- epowerapp.py
+++ epowerapp.py
@@ -60,7 +60,7 @@ with c2:
         
 
         st.text_input('Introduce el código de acceso', type='password', key='acceso')
-        if st.session_state.acceso == st.secrets['KEY_ACCESS'] or st.session_state.acceso == st.secrets['KEY_FREE']:
+        if st.session_state.acceso == st.secrets['KEY_ACCESS'] or st.session_state.acceso == st.secrets['KEY_FREE'] or st.session_state.acceso == st.secrets['KEY_ACCESS2']:
 
             acceso = st.button('🚀 Acceder a la aplicación', type='primary', use_container_width=True, disabled=False)
             st.session_state.usuario_autenticado = True
